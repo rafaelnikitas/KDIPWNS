fd69a1d5c63c6d92ea54d42760693b007e9084fa
Nikitas <47943209+rafaelnikitas@users.noreply.github.com>
2021-10-13 11:24:25 +0300

    Update test.py

--- pipeline/test.py
+++ pipeline/test.py
@@ -5,19 +5,24 @@ from ab_test_cat import ABTestCatMain
 from plot_dates import PlotEventDate
 # process
 event = True
+cat = True
 plot = True
 
 if event:
     ab_df = ABTestEventMain()
     print(ab_df)
 else:
+    print("error")
+    
+if cat:
     ab_df = ABTestCatMain()
     print(ab_df)
+else:
+    print("error")
     
 if plot:
        PlotEventDate()
 else:
        print("error")
-
 
 
